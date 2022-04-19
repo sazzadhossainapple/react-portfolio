be0587dc53903b9933d81f8caeb2ebe836d71607
sazzadhossainapple <sazzadapple81@gmail.com>
2022-04-19 16:45:33 +0600

    Add all my files

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
