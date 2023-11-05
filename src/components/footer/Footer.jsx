5bd51ab817aa56e0cfe22199f5945dd0630f016f
sazzadhossainapple <Sazzad@nextgenitltd.com>
2023-11-05 21:57:07 +0600

    error solve

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
 import IMGFOOTER from '../../asserts/shlogo.png';
 import './footer.css';
 
